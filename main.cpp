79fdaa5695cba80bd53d891b5abda732a40decea
Jens Jákup Egholm Gaardbo <jgaard16@gmail.com>
2017-11-05 18:41:42 +0100

    Added image

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <opencv2/imgproc.hpp>
+#include "opencv2/highgui/highgui.hpp"
 
 using namespace std;
+using namespace cv;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    String imageName( "resizeMe.jpg" );
+    Mat image = imread( imageName, CV_LOAD_IMAGE_GRAYSCALE );
+    if( image.empty() ){
+        cout <<  "Could not open or find the image" << endl;
+        return -1;
+    }
+    imshow("original", image);
+
+
+
+
+
+    waitKey(0);
     return 0;
 }
