306a648a2cee9924a68d7cbbcd8a0e1c76e8c92d
Jens Jákup Egholm Gaardbo <jgaard16@gmail.com>
2017-11-16 14:16:18 +0100

    Added more stuff

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 using namespace cv;
 
 Mat image;
-float scale = 5;
+float scale = 2;
 
-int * resizePixels(int temp[], const int pixels[],int w1,int h1,int w2,int h2) {
+int * resizePixels(int temp[], int pixels[], int w1, int h1, int w2, int h2) {
     int x_ratio = (int)((w1<<16)/w2) +1;
     int y_ratio = (int)((h1<<16)/h2) +1;
     int sX, sY ;
@@ -22,59 +22,77 @@ int * resizePixels(int temp[], const int pixels[],int w1,int h1,int w2,int h2) {
     return temp ;
 }
 
-int * resizeImage(int temp[], const int pixels[] , int w, int h, int w2, int h2) {
+int * resizeImage(int temp[], int pixels[], int w, int h, int w2, int h2) {
     int A, B, C, D, sX, sY, index, gray ;
-    float x_ratio = ((float)(w-1))/w2 ;
-    float y_ratio = ((float)(h-1))/h2 ;
-    float x_diff, y_diff;
-    int offset = 0 ;
+
+    //If we are scaling up. Than the scaling factor is smaller than 1.
+    float x_scaleFactor = scale > 1 ? ((float)(w-1))/w2 : (float)w/w2;
+    float y_scaleFactor = scale > 1 ? ((float)(h-1))/h2 : (float)h/h2;
+
+    float delta_R, delta_C;
+
     for (int i = 0; i < h2; i++) {
-        for (int j=0;j<w2;j++) {
-            sX = (int) (x_ratio * j) ;
-            sY = (int) (y_ratio * i) ;
-            x_diff = (x_ratio * j) - sX;
-            y_diff = (y_ratio * i) - sY;
+
+        for (int j = 0; j < w2; j++) {
+
+            sX = (int) (x_scaleFactor * j) ;
+            sY = (int) (y_scaleFactor * i) ;
+
+            delta_R = (x_scaleFactor * j) - sX;
+            delta_C = (y_scaleFactor * i) - sY;
+
             index = sY * w + sX ;
 
             // range is 0 to 255 thus bitwise AND with 0xff
             A = pixels[index] & 0xff;
-            B = pixels[index+1] & 0xff;
+            B = pixels[index] & 0xff;
             C = pixels[index+w] & 0xff;
             D = pixels[index+w+1] & 0xff;
 
-            // Y = A(1-w)(1-h) + B(w)(1-h) + C(h)(1-w) + Dwh
             gray = (int)(
-                    A*(1-x_diff)*(1-y_diff) +  B*(x_diff)*(1-y_diff) +
-                    C*(y_diff)*(1-x_diff)   +  D*(x_diff*y_diff)
+                    A*(1-delta_R)*(1-delta_C) +
+                    B*(delta_R)*(1-delta_C) +
+                    C*(1-delta_R)*(delta_C) +
+                    D*(delta_R*delta_C)
             ) ;
 
-            temp[offset++] = gray ;
+            temp[i*w2+j] = gray ;
+
         }
+
     }
+
     return temp;
+
 }
 
 int main() {
-    String imageName( "resizeMe.jpg" );
+    String imageName( "resizeMe2.jpg" );
     image = imread( imageName, CV_LOAD_IMAGE_GRAYSCALE );
     if( image.empty() ){
         cout <<  "Could not open or find the image" << endl;
         return -1;
+    } else {
+        cout << imageName << " was loaded!" << endl;
     }
     imshow("original", image);
 
     int w = image.cols;
     int h = image.rows;
 
-    int w2 = static_cast<int>((int)w * scale);
-    int h2 = static_cast<int>((int)h * scale);
+    int w2 = static_cast<int>(w * scale);
+    int h2 = static_cast<int>(h * scale);
 
     int data[w2*h2];
     unsigned char* dataMat = image.data;
-    resizeImage(data, reinterpret_cast<int *>(dataMat), w, h, w2, h2);
 
-    Mat newImage = Mat(h2, w2, CV_8UC1, data);
-    imshow("Resized", newImage);
+    resizePixels(data, reinterpret_cast<int *>(dataMat), w, h, w2, h2);
+    Mat pixelResizedImage = Mat(h2, w2, CV_8U, data);
+    imshow("pixelResizedImage", pixelResizedImage);
+
+    resizeImage(data, reinterpret_cast<int *>(dataMat), w, h, w2, h2);
+    Mat interpolationResizedImage = Mat(h2, w2, CV_8U, data);
+    imshow("interpolationResizedImage", interpolationResizedImage);
 
     Size size(w2, h2);
     Mat openCVScale;
